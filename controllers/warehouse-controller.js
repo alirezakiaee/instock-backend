56099cf61ad876f1ec424a174226ebdd90b06ed7
alirezakiaee <alirezakiaee91@gmail.com>
2024-02-24 15:42:26 -0500

    Clean up comments

--- controllers/warehouse-controller.js
+++ controllers/warehouse-controller.js
@@ -1,19 +1,3 @@
-// const { response } = require('express');
-
-// const knex = require('knex')(require('../knexfile'));
-// const warehouseList = (req,res)=> {
-//     knex('warehouses')
-//     .select('*')
-//     .then(data => {
-//         res.status(200).json(data);
-//     })
-//     .catch(err => {
-//         res.status(404).json({ message: `Error: ${err}` });
-//     });
-// };
-
-// module.exports = {  warehouseList };
-
 const knex = require("knex")(require("../knexfile"));
 
 const warehouseList = (req, res) => {
