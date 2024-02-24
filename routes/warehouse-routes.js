cf2bbdf5fd07a4cd83e3a663334a03c0824c1dee
rebekah schogel <rschogel22@gmail.com>
2024-02-24 17:25:43 -0500

    added new warehouse route and controller

--- routes/warehouse-routes.js
+++ routes/warehouse-routes.js
@@ -1,6 +1,7 @@
-const router = require('express').Router();
-const inventoryController = require('../controllers/warehouse-controller');
+const router = require("express").Router();
+const warehousesController = require("../controllers/warehouse-controller");
 
-router.get('/warehouses', inventoryController.warehouseList);
+router.get("/warehouses", warehousesController.warehouseList);
+router.post("/warehouses", warehousesController.newWarehouse);
 
 module.exports = router;
