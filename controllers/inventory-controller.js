4b2b064914ace50ecc6f312ef8f88dd0855cfd0e
AwdiTech <raby.awdi@gmail.com>
2024-02-26 01:15:21 -0500

    Improved deleteInventory function to handle non-existent items

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -1,29 +1,33 @@
 const { response } = require('express');
 
 const knex = require('knex')(require('../knexfile'));
-const inventoryList = (req,res)=> {
+const inventoryList = (req, res) => {
     knex('inventories')
-    .select('*')
-    .then(data => {
-        res.status(200).json(data);
-    })
-    .catch(err => {
-        res.status(404).json({ message: `Error: ${err}` });
-    });
+        .select('*')
+        .then(data => {
+            res.status(200).json(data);
+        })
+        .catch(err => {
+            res.status(404).json({ message: `Error: ${err}` });
+        });
 };
 
-const deleteInventory = (req, res) => {
+const deleteInventory = async (req, res) => {
     const inventoryId = req.params.id;
 
-    knex('inventories')
-    .where('id', inventoryId)
-    .del()
-    .then(data => {
-        res.status(200).json({ message: `Inventory with id ${inventoryId} deleted` });
-    })
-    .catch(err => {
-        res.status(404).json({ message: `Error: ${err}` });
-    });
+    try {
+        const data = await knex('inventories').where('id', inventoryId).first();
+
+        if (!data) {
+            return res.status(404).json({ message: `Inventory with id ${inventoryId} does not exist` });
+        }
+
+        await knex('inventories').where('id', inventoryId).del();
+        return res.status(200).json({ message: `Inventory with id ${inventoryId} deleted` });
+    }
+    catch (error) {
+        return res.status(500).json({ message: `Error deleting inventory: ${error}` });
+    }
 }
 
 module.exports = { inventoryList, deleteInventory };
