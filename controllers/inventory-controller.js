426523a31fd6aee4a10b57cafaf623278a709115
AwdiTech <raby.awdi@gmail.com>
2024-02-26 00:53:39 -0500

    Implement API Endpoint to DELETE an inventory item using id at /api/inventories/:id

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -12,4 +12,18 @@ const inventoryList = (req,res)=> {
     });
 };
 
-module.exports = {  inventoryList };
+const deleteInventory = (req, res) => {
+    const inventoryId = req.params.id;
+
+    knex('inventories')
+    .where('id', inventoryId)
+    .del()
+    .then(data => {
+        res.status(200).json({ message: `Inventory with id ${inventoryId} deleted` });
+    })
+    .catch(err => {
+        res.status(404).json({ message: `Error: ${err}` });
+    });
+}
+
+module.exports = { inventoryList, deleteInventory };
